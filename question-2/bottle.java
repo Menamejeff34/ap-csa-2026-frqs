13a314e16324c6a9c8408104c2556230bf4414ba
Menamejeff34 <justinbeers1103@gmail.com>
2026-05-19 11:37:24 -0400

    Update bottle.java

--- question-2/bottle.java
+++ question-2/bottle.java
@@ -1,1 +1,23 @@
+public class Bottle
+{
+    private double capacity;
+    private double amount;
 
+    public Bottle(double cap)
+    {
+        capacity = cap;
+        amount = cap;
+    }
+
+    public double updateAmount(double remove)
+    {
+        amount -= remove;
+
+        if (amount < 0.25 * capacity)
+        {
+            amount = capacity;
+        }
+
+        return amount;
+    }
+}
