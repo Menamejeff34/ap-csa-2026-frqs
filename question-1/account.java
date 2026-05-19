4a9664efa30f678e2bc902895032a8a081afcfe7
Menamejeff34 <justinbeers1103@gmail.com>
2026-05-19 11:34:31 -0400

    Update account.java

--- question-1/account.java
+++ question-1/account.java
@@ -1,1 +1,20 @@
+public Account(String requestedName)
+{
+    if (isAvailable(requestedName))
+    {
+        username = requestedName;
+    }
+    else
+    {
+        int num = 1;
+        String possible = requestedName + num;
 
+        while (!isAvailable(possible))
+        {
+            num++;
+            possible = requestedName + num;
+        }
+
+        username = possible;
+    }
+}
