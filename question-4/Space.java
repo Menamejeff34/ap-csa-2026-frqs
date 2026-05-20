f97f2104ea3329d2b4fee980ee16502e52a379e3
Menamejeff34 <justinbeers1103@gmail.com>
2026-05-20 11:50:50 -0400

    Update Space.java

--- question-4/Space.java
+++ question-4/Space.java
@@ -1,1 +1,15 @@
-
+public int getPointsForRow(int targetRow){
+    int sum = 0;
+    boolean sameColor = true;
+    String firstColor = board[targetRow][0].getColor();
+    for (int col = 0; col < board[targetRow].length; col++){
+        sum += board[targetRow][col].getPoints();
+        if (!board[targetRow][col].getColor().equals(firstColor)){
+            sameColor = false;
+        }
+    }
+    if (sameColor){
+        return 2 * sum;
+    }
+    return sum;
+}
