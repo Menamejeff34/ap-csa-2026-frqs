42157762b2e82d8c28b5bc61b4bff91a58b28a1e
Menamejeff34 <justinbeers1103@gmail.com>
2026-05-20 11:44:50 -0400

    Update CourseRecord.java

--- question-3/CourseRecord.java
+++ question-3/CourseRecord.java
@@ -1,1 +1,14 @@
-
+public int moreHistoryThanMathAbsences()
+{
+    int count = 0;
+    for (CourseRecord historyStudent : historyList){
+        String id = historyStudent.getStudentID();
+        for (CourseRecord mathStudent : mathList){
+            if (id.equals(mathStudent.getStudentID()) &&
+                historyStudent.getAbsences() > mathStudent.getAbsences()){
+                count++;
+            }
+        }
+    }
+    return count;
+}
